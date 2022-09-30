aeff310b57687e3d6c72f4c53dac7454cd95e320
sanjeevgith <prajapatisanjiv8@gmail.com.com>
2022-09-30 10:52:38 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ApiService } from './shared/api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeDashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -0,0 +1,102 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from '../shared/api.service';
+import { EmployeeModel } from './employee-dash board.model';
+
+@Component({
+  selector: 'app-employee-dashboard',
+  templateUrl: './employee-dashboard.component.html',
+  styleUrls: ['./employee-dashboard.component.scss']
+})
+export class EmployeeDashboardComponent implements OnInit {
+
+
+formvalue!:FormGroup;
+employeeModelObj: EmployeeModel = new EmployeeModel();
+employeeData!:any;
+showAdd!:boolean;
+showUpdate!:boolean;
+
+  constructor(private fb:FormBuilder,
+    private api:ApiService) { }
+
+  ngOnInit(): void {
+    this.formvalue=this.fb.group({
+      firstname:[''],
+      lastname:[''],
+      email:[''],
+      mobile:[''],
+      salary:['']
+    })
+    this.getAllEmployee();
+  }
+
+
+  clickAddEmploye(){
+    this.formvalue.reset();
+    this.showAdd=true;
+    this.showUpdate=false;
+  }
+  postEmployeeDetails(){
+    this.employeeModelObj.firstname =this.formvalue.value.firstname;
+    this.employeeModelObj.lastname =this.formvalue.value.lastname;
+    this.employeeModelObj.email =this.formvalue.value.email;
+    this.employeeModelObj.mobile =this.formvalue.value.mobile;
+    this.employeeModelObj.salary =this.formvalue.value.salary;
+    this.api.postEmploye(this.employeeModelObj)
+    .subscribe(res=>{
+      console.log(res);
+      alert("employee added succesfully")
+      let ref = document.getElementById('cancel')
+      ref?.click();
+      this.formvalue.reset();
+      this.getAllEmployee();
+    },
+    err=>{
+      alert("something wrong");
+    })
+    
+  }
+
+  getAllEmployee(){
+    this.api.getEmploye()
+    .subscribe(res=>{
+      this.employeeData=res;
+    })
+  }
+
+ deleteEmployee(row:any){
+   this.api.deleteEmploye(row.id)
+   .subscribe(res=>{
+     alert("Employee Deleted")
+     this.getAllEmployee();
+   })
+ }
+
+ onEdit(row:any){
+  this.showAdd=false;
+  this.showUpdate=true;
+   this.employeeModelObj.id=row.id;
+   this.formvalue.controls['firstname'].setValue(row.firstname);
+   this.formvalue.controls['lastname'].setValue(row.lastname);
+   this.formvalue.controls['email'].setValue(row.email);
+   this.formvalue.controls['mobile'].setValue(row.mobile);
+   this.formvalue.controls['salary'].setValue(row.salary);
+ }
+ updateEmployeeDetails(){
+  this.employeeModelObj.firstname =this.formvalue.value.firstname;
+  this.employeeModelObj.lastname =this.formvalue.value.lastname;
+  this.employeeModelObj.email =this.formvalue.value.email;
+  this.employeeModelObj.mobile =this.formvalue.value.mobile;
+  this.employeeModelObj.salary =this.formvalue.value.salary;
+  this.api.updateEmploye(this.employeeModelObj,this.employeeModelObj.id)
+  .subscribe(res=>{
+    alert("Updated Successfully");
+    let ref = document.getElementById('cancel')
+    ref?.click();
+    this.formvalue.reset();
+    this.getAllEmployee();
+  })
+ }
+
+}
